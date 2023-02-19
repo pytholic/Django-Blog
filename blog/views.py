f65cadb53df31ca2c6b2791551585ac9e82bf1ef
pytholic <rajahaseeb147@gmail.com>
2023-02-19 17:47:41 +0900

    passing dummy context data to our template and accesing, loop over it and print

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 
-# posts = [
-#     {
-#         "author": "Pytholic",
-#         "title": "Blog Post 1",
-#         "content": "First psot content",
-#         "date_posted": "February 18, 2023",
-#     },
-#     {
-#         "author": "Jane Doe",
-#         "title": "Blog Post 2",
-#         "content": "Second psot content",
-#         "date_posted": "February 19, 2023",
-#     },
-# ]
+posts = [
+    {
+        "author": "Pytholic",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "February 18, 2023",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "February 19, 2023",
+    },
+]
 
 # How we handle our blog's home page
 def home(request):
-    # context = {"posts": posts}
+    context = {"posts": posts}
     # we can access this context data in out template
     return render(request, template_name="blog/home.html", context=context)
 
