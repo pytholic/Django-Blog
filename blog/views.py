487582d1c197688b0a96f2cecfda245244bdb8e3
pytholic <rajahaseeb147@gmail.com>
2023-02-19 17:39:15 +0900

    applciations and routes

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# posts = [
+#     {
+#         "author": "Pytholic",
+#         "title": "Blog Post 1",
+#         "content": "First psot content",
+#         "date_posted": "February 18, 2023",
+#     },
+#     {
+#         "author": "Jane Doe",
+#         "title": "Blog Post 2",
+#         "content": "Second psot content",
+#         "date_posted": "February 19, 2023",
+#     },
+# ]
+
+# How we handle our blog's home page
+def home(request):
+    # context = {"posts": posts}
+    # we can access this context data in out template
+    return render(request, template_name="blog/home.html", context=context)
+
+
+def about(request):
+    return render(request, template_name="blog/about.html")
